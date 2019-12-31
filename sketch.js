3f9e48b0360a53d0244300a1fd4cecfca53e7afa
RanjiniSarkar <57805617+RanjiniSarkar@users.noreply.github.com>
2019-12-31 20:50:53 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var canvas,backgroundImage,playerCount,database,form,player,game;
+var gameState=0;
+
+
+function setup(){
+    database=firebase.database()
+   canvas= createCanvas(500,500);
+   game = new Game()  
+    game.getState()
+    game.start()
+   
+
+
+  
+}
+
+function draw(){
+    background("white");
+   
+    drawSprites();
+}
+
+    
+
+   
+
